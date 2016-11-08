c8c9d9eb0772d0ac969fa3e1f52187575e46a55c
Chris Marsh <chris.pm@gmail.com>
2016-11-08 23:41:05 +0000

    Added init.sh to make it easier for setting up

---
+++ init.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+here=$(pwd)
+ln -s $here/Xresources ~/.Xresources
+ln -s $here/inputrc ~/.inputrc
+ln -s $here/xinitrc ~/.xinitrc
+ln -s $here/bashrc ~/.bashrc
+ln -s $here/vimrc ~/.vimrc
+ln -s $here/vim ~/.vim
+ln -s $here/fish ~/.config/fish
+ln -s $here/i3 ~/.i3
+ln -s $here/vifm ~/.vifm
