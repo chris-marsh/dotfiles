3c7e92c6356902b58350dbb499139339a52adc84
Chris Marsh <chris.pm@gmail.com>
2017-01-15 09:37:24 +0000

    Switched from fish back to zsh with prezto

--- shell/functions.sh
+++ shell/functions.sh
@@ -34,24 +34,46 @@ psgrep() {
 
 if [ "$TERM" = "linux" ]; then
   /bin/echo -e "
-  \e]P023131a
-  \e]P12e1b23
-  \e]P2452935
-  \e]P3633e4e
-  \e]P48a586d
-  \e]P5b57892
-  \e]P6cdaab8
-  \e]P7ebdfe4
-  \e]P8f4d7c4
-  \e]P9f9dac4
-  \e]PA9bbc50
-  \e]PBe6b7c9
-  \e]PCeab5ca
-  \e]PDbbd689
-  \e]PEc7a5ed
-  \e]PFded2ee
+  \e]P0151515
+  \e]P1ac4142
+  \e]P290a959
+  \e]P3f4bf75
+  \e]P46a9fb5
+  \e]P5aa759f
+  \e]P675b5aa
+  \e]P7d0d0d0
+  \e]P8505050
+  \e]P9ac4142
+  \e]PA90a959
+  \e]PBf4bf75
+  \e]PC6a9fb5
+  \e]PDaa759f
+  \e]PE75b5aa
+  \e]PFf5f5f5
   "
   # get rid of artifacts
   clear
 fi
+# if [ "$TERM" = "linux" ]; then
+#   /bin/echo -e "
+#   \e]P023131a
+#   \e]P12e1b23
+#   \e]P2452935
+#   \e]P3633e4e
+#   \e]P48a586d
+#   \e]P5b57892
+#   \e]P6cdaab8
+#   \e]P7ebdfe4
+#   \e]P8f4d7c4
+#   \e]P9f9dac4
+#   \e]PA9bbc50
+#   \e]PBe6b7c9
+#   \e]PCeab5ca
+#   \e]PDbbd689
+#   \e]PEc7a5ed
+#   \e]PFded2ee
+#   "
+#   # get rid of artifacts
+#   clear
+# fi
 
---
+++ zshrc
@@ -0,0 +1,47 @@
+#
+# Executes commands at the start of an interactive session.
+#
+# Authors:
+#   Sorin Ionescu <sorin.ionescu@gmail.com>
+#
+
+# Source Prezto.
+if [[ -s "${ZDOTDIR:-$HOME}/.zprezto/init.zsh" ]]; then
+  source "${ZDOTDIR:-$HOME}/.zprezto/init.zsh"
+fi
+
+# Customize to your needs...
+source ~/dotfiles/shell/aliases.sh
+
+export PATH=$PATH:~/bin:.
+export EDITOR=vim
+
+if [ "$TERM" = "linux" ]; then
+  prompt agnoster
+  /bin/echo -e "
+  \e]P0151515
+  \e]P1ac4142
+  \e]P290a959
+  \e]P3f4bf75
+  \e]P46a9fb5
+  \e]P5aa759f
+  \e]P675b5aa
+  \e]P7d0d0d0
+  \e]P8505050
+  \e]P9ac4142
+  \e]PA90a959
+  \e]PBf4bf75
+  \e]PC6a9fb5
+  \e]PDaa759f
+  \e]PE75b5aa
+  \e]PFf5f5f5
+  "
+  # get rid of artifacts
+  clear
+fi
+
+if [ "$TERM" = "xterm" ]; then
+    TERM=xterm-256color
+fi
+
+archey3;
